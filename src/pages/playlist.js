968725652a637993583d35439825d92a1b88fec2
Tanner <tannerhubbert@gmail.com>
2023-12-19 08:31:47 -0600

    move embed to its own component

--- src/pages/playlist.js
+++ src/pages/playlist.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
 const Playlist = () => {
-  return (
-    <div className="container pb-5">
-      <h1 className="mt-5 mb-5 text-center">
-        My top songs of 2023 (according to spotify)
-      </h1>
+  function EmbedSpotifyList() {
+    return (
       <div>
         <iframe
+          title="Spotify Playlist"
           src="https://open.spotify.com/embed/playlist/2P6iWlv2MqdEtJ6GErrnXB?utm_source=generator"
           width="100%"
           height={700}
@@ -16,6 +14,14 @@ const Playlist = () => {
           allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
           loading="lazy"></iframe>
       </div>
+    );
+  }
+  return (
+    <div className="container pb-5">
+      <h1 className="mt-5 mb-5 text-center">
+        My top songs of 2023 (according to spotify)
+      </h1>
+      <EmbedSpotifyList />
     </div>
   );
 };
